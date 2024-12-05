9dc39c2922c8f3a6838f2f8600d03993d6cee348
Dhyanesh Janardan Dharmik <dharmikdj@rknec.edu>
2024-12-05 16:37:09 +0530

    last commit

--- todolist/urls.py
+++ todolist/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include  # Use include for app URLs
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('to-do-using-django-git-main-dhyanesh-dharmiks-projects.vercel.app/', admin.site.urls),
     path('', include('tasks.urls')),  # Include URLs from the tasks app
 ]
